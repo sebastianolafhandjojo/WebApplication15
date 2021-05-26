a5da9138cca84aeb89f87689b5d2b6d3c1f8489a
sebastianolafhandjojo <sebastianolafhandjojo@gmail.com>
2021-05-26 21:47:27 +0800

    fetch Environment Variable

---
+++ WebApplication15/Server/Controllers/EnvironmentVariableController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using WebApplication15.Shared;
+using System.Net;
+using System.Diagnostics;
+namespace WebApplication15.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnvironmentVariableController : Controller                  
+    {
+        private readonly ILogger<EnvironmentVariableController> _logger;
+
+        public EnvironmentVariableController(ILogger<EnvironmentVariableController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<EnVar> Get()
+        {
+            List<EnVar> envars = new List<EnVar>();
+            envars.Add(new EnVar { ID = "Process", Value = Process.GetCurrentProcess().ProcessName });
+            
+            string localIpAddress = string.Empty;
+
+            var dns = Dns.GetHostName();
+            var ips = Dns.GetHostAddressesAsync(dns).Result;                
+
+            foreach(IPAddress ip in ips)
+            {
+                envars.Add(new EnVar { ID = "IP", Value = ip.ToString() });
+            }
+            envars.Add(new EnVar { ID = "DNS", Value = dns });
+
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+            {
+                envars.Add(new EnVar() { ID = de.Key.ToString(), Value = de.Value.ToString() });
+            }            
+            return envars.ToArray();
+        }
+    }
+}
---
+++ WebApplication15/Shared/EnVar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication15.Shared
+{
+    public class EnVar
+    {
+        public string ID { get; set; }
+        public string Value { get; set; }
+    }
+}
