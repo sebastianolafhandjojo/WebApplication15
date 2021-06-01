2bfde0b22288bf5a0ea8e0993c7581c7685c5a53
sebastianolafhandjojo <sebastianolafhandjojo@gmail.com>
2021-06-02 00:01:13 +0800

    replace logging with serilog

--- WebApplication15/Server/Program.cs
+++ WebApplication15/Server/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace WebApplication15.Server
 {
@@ -13,11 +14,21 @@ namespace WebApplication15.Server
     {
         public static void Main(string[] args)
         {
+            SetupSerilog();
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void SetupSerilog()
+        {
+            Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
